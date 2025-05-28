833497e90d8036be8b82f7ccbb0086de28588f51
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-05-28 14:26:12 -0500

    A clickable text was created and modifiers were implemented.

--- composeApp/src/commonMain/kotlin/org/example/basic_kmp_comp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/basic_kmp_comp/App.kt
@@ -2,18 +2,24 @@ package org.example.basic_kmp_comp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -25,9 +31,14 @@ import basiccomposecomponents.composeapp.generated.resources.compose_multiplatfo
 fun App() {
     MaterialTheme {
         Box(Modifier.statusBarsPadding()){
-            Button(onClick = {}){
-                Text("Click me!")
-            }
+            Text(
+                text = "Click me!",
+                modifier = Modifier
+                    .clickable { println("Clicked") }
+                    .background(Color.Magenta, shape = RoundedCornerShape(8.dp))
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+
+            )
         }
     }
 }

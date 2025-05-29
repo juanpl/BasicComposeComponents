9ffa79d572669f9c5c7619e0800ace0e7ae8a581
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-05-28 21:57:45 -0500

    Column normal uses

--- composeApp/src/commonMain/kotlin/org/example/basic_kmp_comp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/basic_kmp_comp/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -30,8 +31,17 @@ import basiccomposecomponents.composeapp.generated.resources.compose_multiplatfo
 @Preview
 fun App() {
     MaterialTheme {
-        Box(Modifier.statusBarsPadding()){
+        Column(
+            modifier = Modifier
+                .statusBarsPadding()
+                .background(Color.Yellow)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ){
             MyButton("Click me")
+            MyButton("Click me too")
+            MyButton("Click me again!!")
         }
     }
 }
